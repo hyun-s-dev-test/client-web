6158f7b4d1fc6ceb5347b217cb0d8c84afe73e84
doosanehi <bds0203@gmail.com>
2022-02-28 09:55:35 +0900

    findComponent 생성

---
+++ src/feature/auth/component/FindComponent.jsx
@@ -0,0 +1,6 @@
+import { jsx } from "@emotion/react";
+import { FindWrapper } from "../style/FindWrapper";
+
+export const FindComponent = ({ children }) => {
+  return <FindWrapper>{children}</FindWrapper>;
+};
