775475bbd0d5cdc6630d3af3cd1e5b35015a9c3a
doosanehi <bds0203@gmail.com>
2022-02-28 10:02:11 +0900

    FondWrapper Style

---
+++ src/feature/auth/style/FindWrapper.jsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const FindWrapper = styled.ul`
+  padding: 20px 0 35px;
+  text-align: center;
+  list-style: none;
+  display: block;
+  font-size: 14px;
+  li {
+    position: relative;
+    display: inline-block;
+    margin-left: 10px;
+    marging-right: 10px;
+    a {
+      cursor: pointer;
+    }
+  }
+`;
