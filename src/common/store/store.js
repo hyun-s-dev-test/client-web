5557481682207f210e727ea0350fc85281a0998f
doosanehi <bds0203@gmail.com>
2022-02-28 09:55:20 +0900

    redux store

--- src/common/store/store.js
+++ src/common/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userAuthSlice from "../../feature/auth/state/userAuthSlice";
 import userInfoReducer from "../../feature/auth/state/userInfoSlice";
 
 export const store = configureStore({
   reducer: {
     userInfo: userInfoReducer,
+    userAuth: userAuthSlice,
   },
 });
