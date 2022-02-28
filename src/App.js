ca60180dc2d204f53689fbb34aa0dd55a13d94f3
doosanehi <bds0203@gmail.com>
2022-02-28 09:55:12 +0900

    app.js 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./feature/auth/login/Login";
+import Register from "./feature/auth/register/Register";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Reset } from "styled-reset";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Reset />
-      <Login></Login>
-    </>
+      <Routes>
+        <Route element={<Login />} path="/" />
+        <Route element={<Register />} path="/register"></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
