9cbffe1a335c140ba4246d6738d577baab8e9ce8
doosanehi <bds0203@gmail.com>
2022-02-28 14:08:27 +0900

    Register 페이지 생년월일 컴포넌트 세분화

--- src/feature/auth/register/Register.jsx
+++ src/feature/auth/register/Register.jsx
@@ -3,18 +3,20 @@
 import { jsx, css } from "@emotion/react";
 import React from "react";
 import { useRef } from "react";
-import { Header, LogoBox, InputFormWrapper } from "../style/Auth";
-import { useSelector } from "react-redux";
-import * as A from "../state/userInfoSlice";
+import { Header, LogoBox, InputFormWrapper, StyledWaringMessage } from "../style/Auth";
+import { useDispatch, useSelector } from "react-redux";
+import * as userAuth from "../state/userAuthSlice";
 import { request } from "../../../common/api/api";
 import { ButtonComponent } from "../component/ButtonComponent";
 import { InputComponent } from "../component/InputComponent";
 import { Link } from "react-router-dom";
 import { FindComponent } from "../component/FindComponent";
+import styled from "@emotion/styled";
+import { getDay } from "../util/getDay";
 
 const Login = () => {
   const userSelector = useSelector((state) => {
-    const { id, password } = state.userInfo;
+    const { id, password } = state.userAuth;
     return { id, password };
   });
   const onSubmitHandler = (e) => {
@@ -35,7 +37,7 @@ const Login = () => {
           placeholder="id를 입력하세요"
           title="아이디"
           validator="id"
-          action={A.setId}
+          action={userAuth.setId}
         ></InputComponent>
         <InputComponent
           name="password"
@@ -43,7 +45,7 @@ const Login = () => {
           title="비밀번호"
           type="password"
           validator="password"
-          action={A.setPassword}
+          action={userAuth.setPassword}
         ></InputComponent>
         <InputComponent
           name="name"
@@ -51,7 +53,7 @@ const Login = () => {
           title="이름"
           type="text"
           validator="name"
-          action={A.setName}
+          action={userAuth.setName}
         ></InputComponent>
         <InputComponent
           name="sex"
@@ -59,7 +61,7 @@ const Login = () => {
           title="성별"
           type="text"
           validator=""
-          action={A.setSex}
+          action={userAuth.setSex}
         ></InputComponent>
         <InputComponent
           name="phone"
@@ -67,8 +69,34 @@ const Login = () => {
           title="휴대전화"
           type="text"
           validator="phone"
-          action={A.setSex}
+          action={userAuth.setSex}
         ></InputComponent>
+        <CalenderInput>
+          <InputComponent
+            name="year"
+            placeholder="1999"
+            title="태어난 년"
+            type="text"
+            validator="year"
+            action={userAuth.setYear}
+          ></InputComponent>
+          <InputComponent
+            name="month"
+            placeholder="3"
+            title="월"
+            type="text"
+            validator="month"
+            action={userAuth.setMonth}
+          ></InputComponent>
+          <InputComponent
+            name="day"
+            placeholder="21"
+            title="일"
+            type="text"
+            validator="day"
+            action={userAuth.setDay}
+          ></InputComponent>
+        </CalenderInput>
         <ButtonComponent title="제출" type="submit"></ButtonComponent>
       </InputFormWrapper>
       <FindComponent>
@@ -87,3 +115,35 @@ const Login = () => {
 };
 
 export default Login;
+
+const CalenderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  div:not(:first-of-type) {
+    margin-left: 20px;
+  }
+`;
+
+const CalenderInput = ({ children }) => {
+  const dispatch = useDispatch();
+  const userBirthSelector = useSelector((state) => {
+    const { year, month, day } = state.userAuth;
+    return { year, month, day };
+  });
+  const lastDay = getDay(userBirthSelector.year, userBirthSelector.month);
+  const { year, month, day } = userBirthSelector;
+  //   if (Number(lastDay.day) < Number(userBirthSelector.day)) {
+  //     dispatch(userAuth.setDay(null));
+  //   }
+
+  return (
+    <>
+      <CalenderWrapper>{children}</CalenderWrapper>
+      {Number(lastDay.day) < Number(day) ? (
+        <StyledWaringMessage className="doosan">올바른 날짜가 아닙니다.</StyledWaringMessage>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
