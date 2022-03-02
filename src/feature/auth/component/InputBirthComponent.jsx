ccb8644c0770235e73d2cbefa8fc815989bb460f
doosanehi <bds0203@gmail.com>
2022-03-02 15:50:04 +0900

    생년월일 component

---
+++ src/feature/auth/component/InputBirthComponent.jsx
@@ -0,0 +1,40 @@
+import { jsx } from "@emotion/react";
+import { useState } from "react";
+import { InputComponentWrapper, StyledTitle, StyledInput, StyledWaringMessage } from "../style/Auth";
+import { useDispatch } from "react-redux";
+import { validateInput } from "../util/validateInput";
+
+export const InputBirthComponent = ({
+  name,
+  type,
+  placeholder,
+  title,
+  inputValue,
+  setInputValue,
+  setWarningToggle,
+}) => {
+  const onChangeHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+  const onBlurHandler = (e) => {
+    setWarningToggle(true);
+  };
+  const onFocusHandler = () => {
+    setWarningToggle(false);
+  };
+
+  return (
+    <InputComponentWrapper>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledInput
+        name={name}
+        placeholder={placeholder}
+        value={inputValue}
+        type={type}
+        onChange={onChangeHandler}
+        onFocus={onFocusHandler}
+        onBlur={onBlurHandler}
+      ></StyledInput>
+    </InputComponentWrapper>
+  );
+};
