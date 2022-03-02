0be056f604d3cc5f5846590c8e6c70409be622a7
doosanehi <bds0203@gmail.com>
2022-03-02 15:48:27 +0900

    라우터 연결

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Login from "./feature/auth/login/Login";
 import Register from "./feature/auth/register/Register";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Reset } from "styled-reset";
+// import FindPassword from "./feature/auth/find/FindPassword";
+import FindPw from "./feature/auth/find/FindPw";
+import FindId from "./feature/auth/find/FindId";
+import UserInfo from "./feature/auth/userInfo/UserInfo";
+import UserInfoUpdate from "./feature/auth/userInfo/UserInfoUpdate";
 
 function App() {
   return (
@@ -12,6 +17,10 @@ function App() {
       <Routes>
         <Route element={<Login />} path="/" />
         <Route element={<Register />} path="/register"></Route>
+        <Route element={<FindPw />} path="/find/password"></Route>
+        <Route element={<FindId />} path="/find/id"></Route>
+        <Route element={<UserInfo />} path="/user"></Route>
+        <Route element={<UserInfoUpdate />} path="/user/update"></Route>
       </Routes>
     </BrowserRouter>
   );
