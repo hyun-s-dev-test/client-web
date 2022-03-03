1b3967492408331248eb4351d2953e02b8f96eeb
doosanehi <bds0203@gmail.com>
2022-03-03 11:19:32 +0900

    storybook Link , store 사용 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,19 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { MemoryRouter } from "react-router";
+import { store } from "../src/common/store/store";
+import { Provider } from "react-redux";
+
+addDecorator((story) => <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>);
+
+export const decorators = [
+  (Story) => (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +22,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
