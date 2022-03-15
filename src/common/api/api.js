96bb370a48de0982a07085df79ecbaf252209738
doosanehi <bds0203@gmail.com>
2022-03-15 21:01:11 +0900

    api 오류처리 수정

--- src/common/api/api.js
+++ src/common/api/api.js
@@ -3,78 +3,58 @@ import axios from "axios";
 // const baseURL = process.env.REACT_APP_API_URL;
 
 const requestGet = async (url, data = null) => {
-  try {
-    const res = await axios({
-      // baseURL,
-      method: "get",
-      url,
-      params: data,
-    });
-    console.log(res.data);
-    return res;
-  } catch (err) {
-    console.error(err);
-  }
+  const res = await axios({
+    // baseURL,
+    method: "get",
+    url,
+    params: data,
+  });
+  console.log(res.data);
+  return res;
 };
 
 const requestPost = async (url, data = null) => {
-  try {
-    const res = await axios({
-      // baseURL,
-      method: "post",
-      url,
-      data: data,
-    });
-    console.log(res.data);
-    return res;
-  } catch (err) {
-    console.error(err);
-  }
+  const res = await axios({
+    // baseURL,
+    method: "post",
+    url,
+    data: data,
+  });
+  console.log(res.data);
+  return res;
 };
 
 const requestPut = async (url, data = null) => {
-  try {
-    const res = await axios({
-      // baseURL,
-      method: "put",
-      url,
-      data: data,
-    });
-    console.log(res.data);
-    return res;
-  } catch (err) {
-    console.error(err);
-  }
+  const res = await axios({
+    // baseURL,
+    method: "put",
+    url,
+    data: data,
+  });
+  console.log(res.data);
+  return res;
 };
 
 const requestPatch = async (url, data = null) => {
-  try {
-    const res = await axios({
-      // baseURL,
-      method: "patch",
-      url,
-      data: data,
-    });
-    console.log(res.data);
-    return res;
-  } catch (err) {
-    console.error(err);
-  }
+  const res = await axios({
+    // baseURL,
+    method: "patch",
+    url,
+    data: data,
+  });
+  console.log(res.data);
+  return res;
 };
 
 const requestDelete = async (url, data = null) => {
-  try {
-    const res = await axios({
-      // baseURL,
-      method: "delete",
-      url,
-      data: data,
-    });
-    console.log(res.data);
-    return res;
-  } catch (err) {
-    console.error(err);
-  }
+  const res = await axios({
+    // baseURL,
+    method: "delete",
+    url,
+    data: data,
+  });
+  console.log(res.data);
+  return res;
 };
 
 export const request = {
